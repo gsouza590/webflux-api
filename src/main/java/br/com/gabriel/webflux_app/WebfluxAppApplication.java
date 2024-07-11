90e60fc66bb1c795b9e915cbf22eab20017ac0bc
gsouza590 <gsouza590@gmail.com>
2024-07-11 12:36:37 -0300

    create webflux-app

---
+++ src/main/java/br/com/gabriel/webflux_app/WebfluxAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.webflux_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gabriel/webflux_app/WebfluxAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel.webflux_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
